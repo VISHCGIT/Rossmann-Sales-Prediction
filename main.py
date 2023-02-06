fb5bbe33587e279447b08c2ae805806b34fb5dab
Vishnukumar Cheruku <96327518+VISHCGIT@users.noreply.github.com>
2023-02-07 04:07:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import pickle
 st.header("Rossmann Sales Prediction App")
 
 store_new_df = pd.read_csv(r"store_new_feat.csv")
-
+if st.checkbox('Show Store New Features From Dataframe'):
+    store_new_df
 # load best model [vishnu] not loading the model as some problem in loading best model in this github repository, no issues in local
 #best_xgboost_model = xgb.XGBRegressor()
 #best_xgboost_model.load_model("rassmann_best_model.json")

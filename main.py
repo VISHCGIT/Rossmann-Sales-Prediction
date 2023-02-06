4bb08130605a79b72cd20397b850dcada461df7c
Vishnukumar Cheruku <96327518+VISHCGIT@users.noreply.github.com>
2023-02-07 03:14:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,41 +10,32 @@ import datetime
 import pickle
 st.header("Rossmann Sales Prediction App")
 
-#data = pd.read_csv("https://raw.githubusercontent.com/gurokeretcha/WishWeightPredictionApplication/master/Fish.csv")
 store_new_df = pd.read_csv(r"store_new_feat.csv")
-#https://drive.google.com/file/d/1uTu84MB4Lgkl5WwGbulctw_R69Z-asLW/view?usp=share_link
-#load label encoder
-#encoder = LabelEncoder()
-#encoder.classes_ = np.load('classes.npy',allow_pickle=True)
 
-# load best model
+# load best model [vishnu] not loading the model as some problem in loading best model in this github repository, no issues in local
 #best_xgboost_model = xgb.XGBRegressor()
-#best_xgboost_model.load_model("rassmann_best_model.pkl")
-best_xgboost_model = pickle.load(open('rassmann_best_model.pkl','rb'))
-#open('rassmann_best_model.pkl')
+#best_xgboost_model.load_model("rassmann_best_model.json")
 
-if st.checkbox('Show Training Dataframe'):
-    store_new_df
 
-Store = 112#st.text_input("Store: Enter the Store Number (1 to 1115): ")
+Store = st.text_input("Store: Enter the Store Number (1 to 1115): ")
 date1 = st.date_input("Date: Enter date, on Sales forecast needed:")
-Day =  2#date1.day
-Year =  2016#date1.year
-Promo2SinceYear =  2015#date1.year
-CompetitionOpenSinceYear = 2015#date1.year
-Month = 2#date1.month
-CompetitionOpenSinceMonth = 2#date1.month
-Week = 5#datetime.date(Year, Month, Day).isocalendar().week
-Promo2SinceWeek = 5#datetime.date(Year, Month, Day).isocalendar().week
+Day =  date1.day
+Year =  date1.year
+Promo2SinceYear =  date1.year
+CompetitionOpenSinceYear = date1.year
+Month = date1.month
+CompetitionOpenSinceMonth = date1.month
+Week = datetime.date(Year, Month, Day).isocalendar().week
+Promo2SinceWeek = datetime.date(Year, Month, Day).isocalendar().week
 #1 to 7 (Europe Week start day and end day, Mon and Sun) based on Date.
-DayOfWeek = 2#datetime.date(Year, Month, Day).isoweekday()
-StateHoliday = 1#st.text_input("StateHoliday: If above selected Date is, enter a for public holiday, b for Easter holiday, c for Christmas, 0 for None")
-SchoolHoliday = 0#st.text_input("SchoolHoliday: On above selected Date Sales will be affected, Enter 1 for yes, 0 for No")
-CompetitionDistance = 1000#st.text_input("CompetitionDistance: How far away (meters) competitor from the Store "+str(Store))
-Promo = 1#st.text_input("Promo: Enter a value(0 or 1)")
-Promo2 = 0#st.text_input("Promo2: Enter a value (0 or 1)")
-PerCentDiseaseAffInWeek = 50#st.text_input("PerCentDiseaseAffInWeek: Enter expected % of Disease in that week (0 to 100)")
-PromoInterval = 2#st.selectbox('PromoIntreval: Select one value from list', (['nan', 'Jan,Apr,Jul,Oct', 'Feb,May,Aug,Nov', 'Mar,Jun,Sept,Dec']))
+DayOfWeek = datetime.date(Year, Month, Day).isoweekday()
+StateHoliday = st.text_input("StateHoliday: If above selected Date is, enter a for public holiday, b for Easter holiday, c for Christmas, 0 for None")
+SchoolHoliday = st.text_input("SchoolHoliday: On above selected Date Sales will be affected, Enter 1 for yes, 0 for No")
+CompetitionDistance = st.text_input("CompetitionDistance: How far away (meters) competitor from the Store "+str(Store))
+Promo = st.text_input("Promo: Enter a value(0 or 1)")
+Promo2 = st.text_input("Promo2: Enter a value (0 or 1)")
+PerCentDiseaseAffInWeek = st.text_input("PerCentDiseaseAffInWeek: Enter expected % of Disease in that week (0 to 100)")
+PromoInterval = st.selectbox('PromoIntreval: Select one value from list', (['nan', 'Jan,Apr,Jul,Oct', 'Feb,May,Aug,Nov', 'Mar,Jun,Sept,Dec']))
 StoreType = store_new_df.loc[store_new_df.Store == Store, 'StoreType'].values[0]
 Assortment = store_new_df.loc[store_new_df.Store == Store, 'Assortment'].values[0]
 AvgSalesPerStore = store_new_df.loc[store_new_df.Store == Store, 'AvgSalesPerStore'].values[0]
@@ -65,8 +56,8 @@ Query_data = np.array(Query_data)
 Query_data = Query_data[np.newaxis,:]
 
 
-if st.button('Make Prediction'):
+if st.button('Make Sales Prediction'):
     prediction = 6325
+    #[vishnu] not loaded the model as some problem in loading best model in this github repository, no issues in local, so hard coding as discussed
     #best_xgboost_model.predict(Query_data)
-    #print("final pred", prediction)
     st.write("Sales Prediction for the Store is: "+str(prediction[-1]))
